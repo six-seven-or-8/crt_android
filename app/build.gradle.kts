52d6e4c9c54ac9de80420c3bb3d1b326779fbf59
Six-Seven <cleanservices88@proton.me>
2026-05-06 12:09:42 -0600

    chore: bump versionCode 4, versionName 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "mx.sixseven.crtlineas"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
